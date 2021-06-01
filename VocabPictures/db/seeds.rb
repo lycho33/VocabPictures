c2f42e7dcbadfec0d2dd500141b195bc90633277
lycho33 <cho.lyds@gmail.com>
2021-06-01 13:04:24 -0400

    updated the seed data so it will appear on the api

--- VocabPictures/db/seeds.rb
+++ VocabPictures/db/seeds.rb
@@ -7,9 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 #Images
-mean = Image.create(category: mean, url: 'https://cdn.costumewall.com/wp-content/uploads/2017/03/cruella-de-vil.jpg')
-indifferent = Image.create(category: indifferent, url: 'https://images.squarespace-cdn.com/content/v1/54992035e4b0c49940a59e49/1586714639270-6O27VM796316NPD4R3DH/ke17ZwdGBToddI8pDm48kM67dVXED_yMBjHtlg_uL0sUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcNPDvIAs8f1WUKmWSu3fO4mWtI-U7sMj89S5lCDO4xNISmZH7m9nvOKn--z65CT5h/image-asset.jpeg?format=2500w')
-smart = Image.create(category: smart, url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3100d30-b066-47b5-9972-f34bfebcb82e/de89d31-398603fe-20fa-4f01-8718-8df57b020579.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzMTAwZDMwLWIwNjYtNDdiNS05OTcyLWYzNGJmZWJjYjgyZVwvZGU4OWQzMS0zOTg2MDNmZS0yMGZhLTRmMDEtODcxOC04ZGY1N2IwMjA1NzkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.u-WnmksmImkX9siInvxGh_aUSHmEKng72oD2F6m6nxs')
+mean = Image.create(category: 'mean', url: 'https://cdn.costumewall.com/wp-content/uploads/2017/03/cruella-de-vil.jpg')
+indifferent = Image.create(category: 'indifferent', url: 'https://images.squarespace-cdn.com/content/v1/54992035e4b0c49940a59e49/1586714639270-6O27VM796316NPD4R3DH/ke17ZwdGBToddI8pDm48kM67dVXED_yMBjHtlg_uL0sUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcNPDvIAs8f1WUKmWSu3fO4mWtI-U7sMj89S5lCDO4xNISmZH7m9nvOKn--z65CT5h/image-asset.jpeg?format=2500w')
+smart = Image.create(category: 'smart', url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c3100d30-b066-47b5-9972-f34bfebcb82e/de89d31-398603fe-20fa-4f01-8718-8df57b020579.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MzMTAwZDMwLWIwNjYtNDdiNS05OTcyLWYzNGJmZWJjYjgyZVwvZGU4OWQzMS0zOTg2MDNmZS0yMGZhLTRmMDEtODcxOC04ZGY1N2IwMjA1NzkucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.u-WnmksmImkX9siInvxGh_aUSHmEKng72oD2F6m6nxs')
 
 #Save only Images
 mean.save
